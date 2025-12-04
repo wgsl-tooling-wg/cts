bb41f28271d3402e4140f3bc57d25a3c552f4604
lee mighdoll <lee@underneath.ca>
2025-12-04 11:20:04 -0800

    interim update to current deno
    the deno tests don't work yet, but might after https://github.com/denoland/deno/pull/31163 lands.

--- src/common/runtime/helper/sys.ts
+++ src/common/runtime/helper/sys.ts
@@ -1,26 +1,9 @@
 /* eslint-disable no-process-exit, n/no-process-exit */
 /* eslint-disable @typescript-eslint/no-namespace */
 
-function node() {
-  /* eslint-disable-next-line n/no-restricted-require */
-  const { readFile, existsSync } = require('fs');
-
-  return {
-    type: 'node',
-    readFile,
-    existsSync,
-    args: process.argv.slice(2),
-    cwd: () => process.cwd(),
-    exit: (code?: number | undefined) => process.exit(code),
-  };
-}
-
 declare global {
   namespace Deno {
-    function readFile(
-      path: string,
-      callback?: (error: unknown, data: string) => void
-    ): Promise<Uint8Array>;
+    function readFile(path: string): Promise<Uint8Array>;
     function readFileSync(path: string): Uint8Array;
     const args: string[];
     const cwd: () => string;
@@ -28,26 +11,59 @@ declare global {
   }
 }
 
-function deno() {
-  function existsSync(path: string) {
-    try {
-      Deno.readFileSync(path);
-      return true;
-    } catch (err) {
-      return false;
-    }
-  }
+type ReadFileCallback = (err: { message: string } | null, data: Uint8Array) => void;
 
+interface Sys {
+  type: string;
+  readFile: (path: string, callback: ReadFileCallback) => void;
+  existsSync: (path: string) => boolean;
+  args: string[];
+  cwd: () => string;
+  exit: (code?: number) => never;
+}
+
+const sys = getSys();
+
+function getSys(): Sys {
+  const isDeno = typeof globalThis.Deno !== 'undefined';
+  return isDeno ? denoSys() : nodeSys();
+}
+
+function denoSys(): Sys {
   return {
     type: 'deno',
-    existsSync,
-    readFile: Deno.readFile,
+    existsSync(path: string) {
+      try {
+        Deno.readFileSync(path);
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    readFile(path: string, callback: ReadFileCallback) {
+      Deno.readFile(path).then(
+        data => callback(null, data),
+        err => callback({ message: String(err) }, new Uint8Array())
+      );
+    },
     args: Deno.args,
     cwd: Deno.cwd,
     exit: Deno.exit,
   };
 }
 
-const sys = typeof globalThis.process !== 'undefined' ? node() : deno();
+function nodeSys(): Sys {
+  // require() must be inside this function - Deno 2 doesn't support require()
+  /* eslint-disable-next-line n/no-restricted-require */
+  const fs = require('fs');
+  return {
+    type: 'node',
+    readFile: fs.readFile,
+    existsSync: fs.existsSync,
+    args: process.argv.slice(2),
+    cwd: () => process.cwd(),
+    exit: (code?: number | undefined) => process.exit(code),
+  };
+}
 
 export default sys;
